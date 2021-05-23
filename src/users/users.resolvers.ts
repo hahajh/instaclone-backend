538742e4b32f946826441eba5de84647973cbf75
hahajh <hahajh@gmail.com>
2021-05-23 17:07:06 +0900

    add Computed fields

---
+++ src/users/users.resolvers.ts
@@ -0,0 +1,57 @@
+import { User } from "@prisma/client";
+import client from "../client";
+import { Context } from "../types";
+
+export default {
+    User: {
+        totalFollowing: ({ id }) =>
+            client.user.count({
+                where: {
+                    followers: {
+                        some: {
+                            id
+                        }
+                    }
+                }
+            }),
+        totalFollowers: ({ id }) =>
+            client.user.count({
+                where: {
+                    following: {
+                        some: {
+                            id
+                        }
+                    }
+                }
+            }),
+        isMe: ({ id }: User, _: any, { loggedInUser }: Context) => {
+            if (!loggedInUser) {
+                return false;
+            }
+            return id === loggedInUser.id;
+        },
+        isFollowing: async ({ id }: User, _: any, { loggedInUser }: Context) => {
+            if (!loggedInUser) {
+                return false;
+            }
+            // const exists = await client.user
+            //     .findUnique({ where: { username: loggedInUser.username } })
+            //     .following({
+            //         where: {
+            //             id
+            //         }
+            //     })
+            const exists = await client.user.count({
+                where: {
+                    username: loggedInUser.username,
+                    following: {
+                        some: {
+                            id
+                        }
+                    }
+                }
+            })
+            return Boolean(exists);
+        }
+    }
+}
